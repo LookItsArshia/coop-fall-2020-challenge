ec6fb5f3ab5bd5b5f3a0759c25e7a32a02913ee0
Arshia Mathur <mathur.arshia99@gmail.com>
2020-07-10 15:43:58 -0400

    fixed history, added status for undo

--- solution_python.py
+++ solution_python.py
@@ -5,33 +5,40 @@ class EventSourcer():
     def __init__(self):
         self.value = 0
         self.history = []
-        self.redo = []
+        self.redos = []
+        self.status = 0
 
 
 
     def add(self, num: int):
         self.value += num
-        self.history.append(['+',num])
-        print(self.value)
+        if self.status==0:
+            self.history.append(['+',num])
         
 
     def subtract(self, num: int):
         self.value -= num
-        self.history.append(['-',num])
+        if self.status==0:
+            self.history.append(['-',num])
+        
         
 
     def undo(self):
+        
         temp = self.history.pop()
+        self.status = 1
         if temp[0]=='+':
             self.subtract(temp[1])
-        else:
+        elif temp[0]=='-':
             self.add(temp[1])
-        self.redo.append(temp)
+        self.status = 0
+        self.redos.append(temp)
 
         
 
     def redo(self):
-        temp = self.redo.pop(0)
+
+        temp = self.redos.pop(0)
         if temp[0]=='+':
             self.add(temp[1])
         else:
@@ -41,22 +48,32 @@ class EventSourcer():
 
     def bulk_undo(self, steps: int):
         i = 0
-        for i in range(steps):
+        for i in range(min(len(self.history),steps)):
             self.undo()
         
 
     def bulk_redo(self, steps: int):
         i = 0
-        for i in range(steps):
+        for i in range(min(len(self.redos),steps)):
             self.redo()
         
 
 
 
-value = EventSourcer()
-value.add(3)
-value.undo()
-value.add(7)
-value.subtract((3))
-value.bulk_undo(2)
-value.add(12)
+# value = EventSourcer()
+# value.add(3)
+# value.undo()
+# value.redo()
+# value.add(7)
+# value.add((3))
+# value.bulk_undo(2)
+# value.bulk_redo(2)
+# value.subtract(4)
+# value.undo()
+# value.add(3)
+# value.add(23)
+# value.bulk_redo(3)
+# value.bulk_undo(4)
+
+# # value.add(12)
+# print(value.value)
